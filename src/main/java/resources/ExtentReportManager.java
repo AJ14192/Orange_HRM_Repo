8ac36dbf1c77b94c5b118be6b40efb7191ea833c
Ankit <adjadeja14192@gmail.com>
2024-12-04 15:13:27 +0530

    Updated automation script

---
+++ src/main/java/resources/ExtentReportManager.java
@@ -0,0 +1,27 @@
+package resources;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportManager {
+
+    static ExtentReports extentReport;
+
+    public static ExtentReports getExtentReport() {
+
+        String extentReportPath = System.getProperty("user.dir")+"\\reports\\extentreport.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(extentReportPath);
+        reporter.config().setReportName("TutorialsNinja Automation Results");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extentReport = new ExtentReports();
+        extentReport.attachReporter(reporter);
+        extentReport.setSystemInfo("Operating System","Windows 10");
+        extentReport.setSystemInfo("Tested By","Arun Motoori");
+
+        return extentReport;
+
+    }
+
+}
